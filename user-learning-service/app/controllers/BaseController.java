d40bd534815f898156aab681dba35377383d3737
anmol2302 <anmolgupta23021997@gmail.com>
2019-06-03 15:57:08 +0530

    play csrf filter disabled

--- user-learning-service/app/controllers/BaseController.java
+++ user-learning-service/app/controllers/BaseController.java
@@ -16,7 +16,7 @@ import java.util.concurrent.CompletionStage;
 public class BaseController extends Controller {
 
     public String getDummyResponse() {
-        String dummyResponse = "{\"id\":\"api.private.user.migrate\",\"ver\":\"v1\",\"ts\":\"2019-01-17 16:53:26:286+0530\",\"params\":{\"resmsgid\":null,\"msgid\":\"8e27cbf5-e299-43b0-bca7-8347f7ejk5abcf\",\"err\":null,\"status\":\"success\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"response\":{\"response\":\"SUCCESS\",\"errors\":[]}}}";
+        String dummyResponse = "{\"id\":\"api.user.200ok\",\"ver\":\"v1\",\"ts\":\"2019-01-17 16:53:26:286+0530\",\"params\":{\"resmsgid\":null,\"msgid\":\"8e27cbf5-e299-43b0-bca7-8347f7ejk5abcf\",\"err\":null,\"status\":\"success\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"response\":{\"response\":\"SUCCESS\",\"errors\":[]}}}";
         return dummyResponse;
     }
 
