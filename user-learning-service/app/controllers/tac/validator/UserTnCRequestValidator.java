59034414dcab25cde7bd66117377d943e10ce361
anmol2302 <anmolgupta23021997@gmail.com>
2019-06-03 16:15:27 +0530

    java doc for validator

--- user-learning-service/app/controllers/tac/validator/UserTnCRequestValidator.java
+++ user-learning-service/app/controllers/tac/validator/UserTnCRequestValidator.java
@@ -5,8 +5,16 @@ import org.sunbird.common.request.BaseRequestValidator;
 import org.sunbird.common.request.Request;
 import org.sunbird.common.responsecode.ResponseCode;
 
+/**
+ * This is a validation class specific to Terms and Condition validation
+ */
 public class UserTnCRequestValidator extends BaseRequestValidator {
 
+
+  /**
+   * This method is used  to validate the Terms and condition request.
+   * @param request
+   */
   public void validateTnCRequest(Request request) {
     validateParam(
         (String) request.get(JsonKey.VERSION),
