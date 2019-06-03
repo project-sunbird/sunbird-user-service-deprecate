0858145661b20e842ded397c2f29eca77378cbbd
anmol2302 <anmolgupta23021997@gmail.com>
2019-06-03 16:23:27 +0530

    user read base api moved to user profile read controller

--- user-learning-service/app/controllers/usermanagement/UserController.java
+++ user-learning-service/app/controllers/usermanagement/UserController.java
@@ -2,6 +2,7 @@ package controllers.usermanagement;
 
 import controllers.BaseController;
 import play.mvc.*;
+
 import java.util.concurrent.CompletionStage;
 
 
@@ -29,38 +30,5 @@ public class UserController extends BaseController {
         return handelRequest();
     }
 
-
-    /**
-     * This action method we used to retrive user details on the basis of login Id
-     *
-     * @return CompletionStage of get User by login Id api result
-     */
-    public CompletionStage<Result> getUserByLoginId() {
-        return handelRequest();
-
-    }
-
-    /**
-     * This action method we used to retrive user details on the basis of object.
-     *
-     * @return CompletionStage of get User by Key api result
-     */
-
-    public CompletionStage<Result> getUserByKey(String idType, String id) {
-        return handelRequest();
-
-
-    }
-
-    /**
-     * This method is used to search user on the basis of filters
-     *
-     * @return Return a promise of user response
-     */
-
-    public CompletionStage<Result> searchUser() {
-        return handelRequest();
-
-    }
 
 }
--- user-learning-service/app/controllers/usermanagement/UserProfileReadController.java
+++ user-learning-service/app/controllers/usermanagement/UserProfileReadController.java
@@ -57,4 +57,38 @@ public class UserProfileReadController extends BaseController {
     public CompletionStage<Result> getUserByIdV2(String userId) {
         return handelRequest();
     }
+
+    /**
+     * This action method we used to retrive user details on the basis of login Id
+     *
+     * @return CompletionStage of get User by login Id api result
+     */
+    public CompletionStage<Result> getUserByLoginId() {
+        return handelRequest();
+
+    }
+
+    /**
+     * This action method we used to retrive user details on the basis of object.
+     *
+     * @return CompletionStage of get User by Key api result
+     */
+
+    public CompletionStage<Result> getUserByKey(String idType, String id) {
+        return handelRequest();
+
+
+    }
+
+    /**
+     * This method is used to search user on the basis of filters
+     *
+     * @return Return a promise of user response
+     */
+
+    public CompletionStage<Result> searchUser() {
+        return handelRequest();
+
+    }
+
 }
